ca64e3d62b2faadebebde404bb511af5ef68b91f
Nikitha Bhonsle G <54900544+Nikitha-Bhonsle-G@users.noreply.github.com>
2020-04-30 12:14:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+const mongoose = require('mongoose')
+var port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+
+const mongoURI = 'mongodb://localhost:27017/OKR'
+
+mongoose
+  .connect(
+    mongoURI,
+    { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+     }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+var Employee = require('./routes/Employees')
+
+app.use('/Employee', Employee)
+
+app.listen(port, function() {
+  console.log('Server is running on port: ' + port)
+})
